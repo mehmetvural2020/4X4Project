2b1aae4308585c137680028f3905e151c2157c2b
mehmetvural2020 <mehmetvurall@gmail.com>
2020-11-19 02:24:19 +0700

    I created updateTaskDueDateById() method in UpdateTask.java file. And now we can update a task due date by task id.

--- src/UpdateTask.java
+++ src/UpdateTask.java
@@ -1,3 +1,5 @@
+import java.time.LocalDate;
+
 public class UpdateTask {
     public static void deleteTasksById() {
         System.out.print("Please enter the Task id to remove from the Task list : ");
@@ -10,7 +12,7 @@ public class UpdateTask {
     public static void updateTaskNameById() {
         System.out.print("Please enter the Task id to update the Task name : ");
         int id = Integer.parseInt(Main.scanner.nextLine());
-        System.out.println("Please enter the new Task name to replace with previous name : ");
+        System.out.print("Please enter the new Task name to replace with previous name : ");
         String newTaskName = Main.scanner.nextLine();
 
         int checkId = -1;
@@ -28,6 +30,22 @@ public class UpdateTask {
     }
 
     public static void updateTaskDueDateById() {
+        System.out.print("Please enter the Task id to update the Task Due Date : ");
+        int id = Integer.parseInt(Main.scanner.nextLine());
+
+        System.out.print("\tEnter the new Task Due Date in the following format (yyyy-MM-dd) : ");
+        String newDate = Main.scanner.nextLine();
+        LocalDate newDueDate = LocalDate.parse(newDate, Main.formatter);
+
+        int checkId = -1;
+        for (int i = 0; i < Main.taskList.size(); i++){
+            if(Main.taskList.get(i).id == id){
+                Main.taskList.get(i).setDueDate(newDueDate);
+                Main.taskList.set(i,Main.taskList.get(i));
+                checkId = 1;
+            }
+        }
+        if(checkId == -1) System.out.println("There is no this Task id in the Task list.");
     }
 
     public static void updateTask() {
